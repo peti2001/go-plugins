6e4dc6b8daa2af1b45f5b0013038de6aca7380da
Peter Karakas <karakaspeter@webshopexperts.hu>
2017-11-15 23:36:05 +0100

    Temporary solution. The proto type is hard coded.

--- broker/rabbitmq/ack_channel.go
+++ broker/rabbitmq/ack_channel.go
@@ -1,27 +1,46 @@
 package rabbitmq
 
-import "github.com/streadway/amqp"
+import (
+	"sync"
+
+	proto "github.com/golang/protobuf/proto"
+	"github.com/streadway/amqp"
+
+	requestQueueProto "bitbucket.com/peti2001/candidate_parser/request/proto/requestQueue"
+)
 
 var AckChannel channel
 
 type channel struct {
-	ch map[string]chan amqp.Delivery
+	//ch map[string]chan amqp.Delivery
+	ch map[string]amqp.Delivery
+	sync.Mutex
 }
 
 func NewAckChannel() channel {
 	return channel{
-		ch: make(map[string]chan amqp.Delivery),
+		make(map[string]amqp.Delivery),
+		sync.Mutex{},
 	}
 }
 
 func (c *channel) AddDelivery(topic string, d amqp.Delivery) {
-	if _, ok := c.ch[topic]; !ok {
-		c.ch[topic] = make(chan amqp.Delivery)
+	var msg requestQueueProto.AddToQueueRequest
+	proto.Unmarshal(d.Body, &msg)
+
+	c.Lock()
+	if _, ok := c.ch[msg.Url]; !ok {
+		c.ch[msg.Url] = d
 	}
-	c.ch[topic] <- d
+	c.Unlock()
 }
 
 //TODO handle timeout, if the handler cannot ack in time
-func (c *channel) AckDelivery(topic string) {
-	(<-c.ch[topic]).Ack(false)
+func (c *channel) AckDelivery(topic, url string) {
+	c.Lock()
+	if d, ok := c.ch[url]; ok {
+		d.Ack(false)
+		delete(c.ch, url)
+	}
+	c.Unlock()
 }
